ca1573ffc68e87c53c6d8f3cfee004bf818817a5
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-11-19 16:26:50 +0100

    end

--- JS/script.js
+++ JS/script.js
@@ -15,20 +15,23 @@ console.log('Età del passeggero', EtaPasseggero);
 // 3.calcola il prezzo del biglietto per il numero di km
 let PrezzoBigliettoBase = NumeroKm * 0.21;
 console.log('Prezzo dei chilometri in euro', PrezzoBigliettoBase, '€');
-// 4.applicare le due varianti degli sconti
+// 4.applicare le due varianti degli sconti e proiettarle a schermo
 if (EtaPasseggero < 18) {
   let PrezzoJunior = (PrezzoBigliettoBase * 20) / 100;
-  PrezzoJunior = PrezzoBigliettoBase - PrezzoJunior;
+  PrezzoJunior = parseInt(PrezzoBigliettoBase - PrezzoJunior).toFixed(2);
   console.log('prezzo junior', PrezzoJunior, '€');
-  document.getElementById('Prezzo').innerHTML = PrezzoJunior;
+  // proiezione a schermo
+  document.getElementById('Prezzo').innerHTML = 'Questo è il prezzo ' + PrezzoJunior + ' €';
 } else if (EtaPasseggero > 65) {
   let PrezzoSenior = (PrezzoBigliettoBase * 40) / 100;
-  PrezzoSenior = PrezzoBigliettoBase - PrezzoSenior;
-  console.log('prezzo senior', PrezzoSenior, '€');
-  document.getElementById('Prezzo').innerHTML = PrezzoSenior;
+  PrezzoSenior = parseInit(PrezzoBigliettoBase - PrezzoSenior).toFixed(2);
+  console.log('prezzo senior', PrezzoSenior, ' €');
+  // proiezione a schermo
+  document.getElementById('Prezzo').innerHTML = 'Questo è il prezzo ' + PrezzoSenior + ' €';
 } else {
   console.log('prezzo intero', PrezzoBigliettoBase);
-  document.getElementById('Prezzo').innerHTML = PrezzoBigliettoBase;
+  // proiezione a schermo
+  document.getElementById('Prezzo').innerHTML = 'Questo è il prezzo ' + PrezzoBigliettoBase + ' €';
 }
-// 5.proiettare a schermo il prezzo
+
 
