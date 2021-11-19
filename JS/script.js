a1c9685db52f197bb3c53f010d797d71ad17879d
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-11-19 16:51:37 +0100

    end

--- JS/script.js
+++ JS/script.js
@@ -18,13 +18,13 @@ console.log('Prezzo dei chilometri in euro', PrezzoBigliettoBase, '€');
 // 4.applicare le due varianti degli sconti e proiettarle a schermo
 if (EtaPasseggero < 18) {
   let PrezzoJunior = (PrezzoBigliettoBase * 20) / 100;
-  PrezzoJunior = parseInt(PrezzoBigliettoBase - PrezzoJunior).toFixed(2);
+  PrezzoJunior = (PrezzoBigliettoBase - PrezzoJunior).toFixed(2);
   console.log('prezzo junior', PrezzoJunior, '€');
   // proiezione a schermo
   document.getElementById('Prezzo').innerHTML = 'Questo è il prezzo ' + PrezzoJunior + ' €';
 } else if (EtaPasseggero > 65) {
   let PrezzoSenior = (PrezzoBigliettoBase * 40) / 100;
-  PrezzoSenior = parseInt(PrezzoBigliettoBase - PrezzoSenior).toFixed(2);
+  PrezzoSenior = (PrezzoBigliettoBase - PrezzoSenior).toFixed(2);
   console.log('prezzo senior', PrezzoSenior, ' €');
   // proiezione a schermo
   document.getElementById('Prezzo').innerHTML = 'Questo è il prezzo ' + PrezzoSenior + ' €';
